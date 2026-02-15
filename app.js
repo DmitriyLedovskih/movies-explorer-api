0f405128adeae7059f8e313026b28d30f32f64fb
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2026-02-15 14:39:01 +0700

    fix

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const { errors } = require('celebrate');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const error = require('./middlewares/error');
-const router = require('./routes');
-const { errorLogger, requestLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
-const limiter = require('./middlewares/limiter');
+require("dotenv").config();
+const express = require("express");
+const { errors } = require("celebrate");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const error = require("./middlewares/error");
+const router = require("./routes");
+const { errorLogger, requestLogger } = require("./middlewares/logger");
+const cors = require("./middlewares/cors");
+const limiter = require("./middlewares/limiter");
 
-const { PORT = 3000, DB_LINK = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_LINK = "mongodb://127.0.0.1:27017/bitfilmsdb" } =
+  process.env;
 
 mongoose.connect(DB_LINK);
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,26 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { ValidationError } = require('mongoose').Error;
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ValidationError } = require("mongoose").Error;
+const User = require("../models/user");
 const {
-  CREATE_STATUS, UNIQUE_STATUS, incorrectDataMessage, exitMessage, emailBusyMessage,
-} = require('../utils/constants');
-const BadRequestError = require('../errors/BadRequestError');
-const ConflictError = require('../errors/ConflictError');
-const sendUser = require('../utils/sendUser');
+  CREATE_STATUS,
+  UNIQUE_STATUS,
+  incorrectDataMessage,
+  exitMessage,
+  emailBusyMessage,
+} = require("../utils/constants");
+const BadRequestError = require("../errors/BadRequestError");
+const ConflictError = require("../errors/ConflictError");
+const sendUser = require("../utils/sendUser");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const updateData = async (req, res, next) => {
   try {
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      req.body,
-      { new: true, runValidators: true },
-    );
+    const user = await User.findByIdAndUpdate(req.user._id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     sendUser(res, user);
   } catch (err) {
     if (err instanceof ValidationError) {
@@ -40,11 +43,7 @@ const getMe = async (req, res, next) => {
 };
 
 const createUser = async (req, res, next) => {
-  const {
-    name,
-    email,
-    password,
-  } = req.body;
+  const { name, email, password } = req.body;
   try {
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({
@@ -72,22 +71,24 @@ const login = async (req, res, next) => {
     const user = await User.findUserByCredentials(email, password);
     const token = jwt.sign(
       { _id: user._id },
-      NODE_ENV !== 'production' ? 'some-key' : JWT_SECRET,
-      { expiresIn: '7d' },
+      NODE_ENV !== "production" ? "some-key" : JWT_SECRET,
+      { expiresIn: "7d" },
     );
 
-    res.cookie('token', token, {
-      maxAge: 3600000 * 24 * 7,
-      httpOnly: true,
-      sameSite: true,
-    }).send({ email });
+    res
+      .cookie("token", token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: "lax",
+      })
+      .send({ email });
   } catch (err) {
     next(err);
   }
 };
 
 const signOut = (req, res) => {
-  res.clearCookie('token').send({ message: exitMessage });
+  res.clearCookie("token").send({ message: exitMessage });
 };
 
 module.exports = {
